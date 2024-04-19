960ff5e8bd95cdb45fd2717c3325515cf92a879d
nallume <hoxy910@gmail.com>
2024-04-19 09:37:36 +0900

    new

---
+++ src/main/java/com/pingpingz/web/PingpingzApplication.java
@@ -0,0 +1,13 @@
+package com.pingpingz.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpingzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpingzApplication.class, args);
+	}
+
+}
